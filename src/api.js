05a3e3316e5797870f0dabf581ef025a91607a83
unknown <chirashri261004@gmail.com>
2026-02-16 20:22:09 +0530

    Updated API URL for production

--- src/api.js
+++ src/api.js
@@ -1,18 +1,35 @@
-// src/api.js
+// // src/api.js
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// // Attach token automatically
+// API.interceptors.request.use((req) => {
+//   const token = localStorage.getItem("token");
+
+//   if (token) {
+//     req.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return req;
+// });
+
+// export default API;
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://netflix-clone-3jd2.onrender.com/api",
 });
 
-// Attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
-
   return req;
 });
 
